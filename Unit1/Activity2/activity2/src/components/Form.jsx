6e2507a252d77b217a16bd76fd7ea3c14f4eac2a
sierrahowe1 <sierra.howe@unb.ca>
2026-01-16 13:10:30 -0400

    Adding newline

--- Unit1/Activity2/activity2/src/components/Form.jsx
+++ Unit1/Activity2/activity2/src/components/Form.jsx
@@ -41,9 +41,9 @@ export default function Form() {
     return (
         <form>
             <InputField label="Name" placeholder="Enter your name..." value={form.name} name="name" onChange={handleChange} />
-
+            <br />
             <InputField label="Email" placeholder="Enter your email..." value={form.email} name="email" onChange={handleChange} />
-            
+            <br />
             <InputField label="Phone Number" placeholder="Enter your phone number..." value={form.phoneNumber} name="phoneNumber" onChange={handleChange} />
 
             <Rating name="rating" value={form.rating} onChange={(e, newValue) => {setForm((prev) => ({ ...prev, rating: newValue}));}} />
