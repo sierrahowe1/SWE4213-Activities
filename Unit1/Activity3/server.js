a7e897f385d310a1d85a32d854dae3474a67ee33
sierrahowe1 <sierra.howe@unb.ca>
2026-01-19 09:18:44 -0400

    Adding 3.2

--- Unit1/Activity3/server.js
+++ Unit1/Activity3/server.js
@@ -1,9 +1,22 @@
-const express = require('express')
-const app  = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) =>  {
-    res.send('Hello World!')
+app.use(express.json());
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+// TODO: Add products router
+const productsRouter = require("./routes/products");
+app.use("/products", productsRouter);
+
+productsRouter.get("/", (req, res) => {
+    res.json(products)
 });
 
-app.listen(port)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+ 
